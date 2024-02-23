206864a5e05a0818b36ada27bb61f5c263b441ef
25dlee <136829451+25dlee@users.noreply.github.com>
2024-02-23 10:02:05 +0100

    attempt 1 enemy AI

--- TheSlimeAlchemist/Assets/Scripts/BasicEnemy.cs
+++ TheSlimeAlchemist/Assets/Scripts/BasicEnemy.cs
@@ -7,18 +7,42 @@ public class BasicEnemy : MonoBehaviour
     public HealthBarScript healthBarScript;
     Rigidbody2D rb;
     public GameObject explosion;
+
+    public GameObject pointA;
+    public GameObject pointB;
+    private Transform currentPoint;
+    public float speed;
+    //private Animator anim;
+
     
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>(); 
+        rb = GetComponent<Rigidbody2D>();
+        currentPoint = pointB.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Vector2 point = currentPoint.position - transform.position;
+        if(currentPoint == pointB.transform)
+        {
+            rb.velocity = new Vector2(speed, 0);
+        } else
+        {
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if(Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointB.transform)
+        {
+            currentPoint = pointA.transform;
+        }
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f && currentPoint == pointA.transform)
+        {
+            currentPoint = pointB.transform;
+        }
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
