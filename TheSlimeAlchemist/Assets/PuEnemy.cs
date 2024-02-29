53b4a340be804a00c66195df3a8aef6305c19d7a
Rahartos <126924844+Rahartos@users.noreply.github.com>
2024-03-01 00:43:35 +0100

    yeet

--- TheSlimeAlchemist/Assets/PuEnemy.cs
+++ TheSlimeAlchemist/Assets/PuEnemy.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PuEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+     public float speed = 5f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Move the object to the right
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the colliding object has the tag "box"
+        if (other.CompareTag("EnemyDespond"))
+        {
+            // Destroy the game object
+            Destroy(gameObject);
+        }
     }
 }
