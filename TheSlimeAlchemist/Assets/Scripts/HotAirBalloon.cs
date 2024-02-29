6e77c1a57932ac4008504db329e35d76d4c1c434
eheng0401 <102563803+eheng0401@users.noreply.github.com>
2024-02-29 14:19:21 +0100

    .

---
+++ TheSlimeAlchemist/Assets/Scripts/HotAirBalloon.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotAirBalloon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // when activated by helium, move balloon up
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
