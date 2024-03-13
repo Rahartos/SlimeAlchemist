fa72325e976d0517af06589c2428cfa788d5b75e
Rahartos <126924844+Rahartos@users.noreply.github.com>
2024-03-13 15:46:33 +0100

    working on crafting

--- TheSlimeAlchemist/Assets/MixingDisplay.cs
+++ TheSlimeAlchemist/Assets/MixingDisplay.cs
@@ -13,37 +13,59 @@ public class MixingDisplay : MonoBehaviour
     static List<GameObject> childSlots = new List<GameObject>();
     static Dictionary<int, GameObject> itemsDisplayed = new Dictionary<int, GameObject>();
 
+    public GameObject icon;
+
+    static List<string> recipes = new List<string>();
+
     void Start()
     {
         // Iterate through all children of the parent object
         foreach (Transform child in transform)
         {
             // Check if the child has an Image component
-            GameObject imageComponent = child.GetComponent<GameObject>();
-            if (imageComponent != null)
+            GameObject myChild = child.GetComponent<GameObject>();
+            if (myChild != null)
             {
                 // Add the Image component to the list
-                childSlots.Add(imageComponent);
+                childSlots.Add(myChild);
             }
         }
     }
 
     void Update()
+{
+    for (int i = 0; i < mixingInventory.Container.Count && i < childSlots.Count; i++)
     {
-        
-        for (int i = 0; i < mixingInventory.Container.Count && i < childSlots.Count; i++)
+        // Check if the item is present in the mixing inventory and has a positive amount
+        if (mixingInventory.Container[i].item != null && mixingInventory.Container[i].amount > 0)
         {
-            Image itemImage = childSlots[i].GetComponentsInChildren<Image>()[1];
-            
-            //int temp amount = 
-
-            // Check if the item has an icon before assigning it
-            if (mixingInventory.Container[i].item.icon != null)
+            // Check if the item is already displayed
+            if (!itemsDisplayed.ContainsKey(i))
             {
-                itemImage.sprite = mixingInventory.Container[i].item.icon;
+                // Instantiate the icon and set it as a child of the corresponding child slot
+                GameObject obj = Instantiate(icon, childSlots[i].transform);
+                Image itemImage = obj.GetComponent<Image>(); // Get the Image component of the instantiated icon
+                
+                if (itemImage != null)
+                {
+                    itemImage.sprite = mixingInventory.Container[i].item.icon;
+                }
+
+                // Add the instantiated icon to the dictionary with the corresponding index
+                itemsDisplayed.Add(i, obj);
+            }
+        }
+        else
+        {
+            // If the item is not present or has a zero amount, remove it from the displayed items
+            if (itemsDisplayed.ContainsKey(i))
+            {
+                Destroy(itemsDisplayed[i]); // Destroy the icon GameObject
+                itemsDisplayed.Remove(i); // Remove the entry from the dictionary
             }
         }
     }
+}
 
 
     //  int iconCounter = 0;
